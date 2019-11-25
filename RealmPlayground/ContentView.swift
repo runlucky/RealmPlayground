8fbf102d5384ad61ddda98c0b4a71d87208da4ab
Kakeru Fukuda <stand.by.we@gmail.com>
2019-11-25 21:28:53 +0900

    ガワ

---
+++ RealmPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealmPlayground
+//
+//  Created by 福田走 on 2019/11/25.
+//  Copyright © 2019 福田走. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
